40bcee5170b6ec07f45bae406adc5f4a62a3a3c9
kotteletfisk <ostekugle@gmail.com>
2025-02-28 10:24:11 +0100

    added cs env

---
+++ app/Program.cs
@@ -0,0 +1,17 @@
+﻿using Npgsql;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+var connString = "Host=127.0.0.1;Username=postgres;Password=postgres;Database=esport";
+
+await using var conn = new NpgsqlConnection(connString);
+await conn.OpenAsync();
+
+// Retrieve all rows
+await using (var cmd = new NpgsqlCommand("SELECT username FROM players", conn))
+await using (var reader = await cmd.ExecuteReaderAsync())
+{
+while (await reader.ReadAsync())
+    Console.WriteLine(reader.GetString(0));
+}
